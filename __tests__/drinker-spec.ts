3ae3b224c753d2443db082221451a6e9e51ff3c1
James Fox <james.fox@datagility.co.uk>
2019-02-26 13:31:41 +0000

    drinker tests

---
+++ __tests__/drinker-spec.ts
@@ -0,0 +1,49 @@
+import 'mocha';
+import 'mocha-sinon';
+import * as chai from 'chai';
+import * as sinon from 'sinon';
+
+import IConsumptionCalculator from '../src/interface/iconsumptioncalculator';
+import IRoundEvaluator from '../src/interface/iroundevaluator';
+import IRoundTrigger from '../src/interface/iroundtrigger';
+
+import Drinker from '../src/model/drinker';
+import Drinkers from '../src/model/drinkers';
+import RoundEvaluator from '../src/roundevaluator';
+import RoundResult from '../src/model/roundresult';
+
+const expect = chai.expect;
+
+describe('Drinker', () => {
+  describe('ctor', () => {
+    it('should throw an exception if consumptioncalculator is undefined', () => {
+      expect(() => {
+        let consumptionCalculator: IConsumptionCalculator;
+        const sut = new Drinker(consumptionCalculator);
+      }).to.throw(ReferenceError);
+    });
+  });
+
+  describe('drink', () => {
+    const calculatorMock: IConsumptionCalculator = <IConsumptionCalculator>{};
+
+    it('should call consumption calculator if drink not empty', () => {
+      calculatorMock.calculate = sinon.stub().returns(80);
+
+      const sut = new Drinker(calculatorMock);
+      sut.currentDrinkLevel = 100;
+      sut.drink();
+
+      expect((<sinon.SinonStub>calculatorMock.calculate).calledOnce).to.be.true;
+    });
+
+    it('should not call consumption calculator if drink is empty', () => {
+      calculatorMock.calculate = sinon.stub().returns(80);
+
+      const sut = new Drinker(calculatorMock);
+      sut.drink();
+
+      expect((<sinon.SinonStub>calculatorMock.calculate).called).to.be.false;
+    });
+  });
+});
