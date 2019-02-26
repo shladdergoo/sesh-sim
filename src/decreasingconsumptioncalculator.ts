eda9febc3872f9c35d59cc3d59e3d530a8d86e5a
James Fox <james.fox@datagility.co.uk>
2019-02-26 10:02:07 +0000

    remove unused imports

--- src/decreasingconsumptioncalculator.ts
+++ src/decreasingconsumptioncalculator.ts
@@ -1,24 +1,19 @@
 import IConsumptionCalculator from './interface/iconsumptioncalculator';
 
-import ConsumptionResult from './model/consumptionresult';
-import Drinkers from './model/drinkers';
-
 export class DecreasingConsumptionCalculator implements IConsumptionCalculator {
   public calculate(
     currentDrinkLevel: number,
     drinkerIteration: number
   ): number {
     const consumptionRate: number = 20;
-    let consumptionModifier: number = (drinkerIteration - 1);
+    let consumptionModifier: number = drinkerIteration - 1;
 
-    if(consumptionModifier >= consumptionRate) {
+    if (consumptionModifier >= consumptionRate) {
       consumptionModifier = consumptionRate - 1;
     }
 
     currentDrinkLevel =
-      currentDrinkLevel -
-      consumptionRate +
-      consumptionModifier;
+      currentDrinkLevel - consumptionRate + consumptionModifier;
 
     if (currentDrinkLevel < 0) {
       currentDrinkLevel = 0;
